e15191a2fc0dce5fe0b5519090c59079219b62b3
Demetrius Hernandez <demy.hernandez00@gmail.com>
2021-03-29 14:44:19 -0600

    added button,buzzer, and blink functionality

--- project/test/main.c
+++ project/test/main.c
@@ -1,15 +1,15 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
-//#include "switches.h"
+#include "switches.h"
 #include "buzzer.h"
 
 int main(void) {
   configureClocks();    /* setup master oscillator, CPU & peripheral clocks */
   led_init();
   buzzer_init();
-  buzzer_set_period(1000);
-  //switch_init();
+  //buzzer_set_period(1000);
+  switch_init();
 
   enableWDTInterrupts();    /* enable periodic interrupt */
 
--- project/test/wdInterruptHandler.c
+++ project/test/wdInterruptHandler.c
@@ -1,11 +1,30 @@
 #include <msp430.h>
 #include "stateMachines.h"
+#include "led.h"
+#include "buzzer.h"
+#include "switches.h"
+
+char button_state = 0;
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  
+  if (++blink_count == 125 && button_state == 0) {
     state_advance();
     blink_count = 0;
+    //adding this
+    buzzer_set_period(1000);
   }
+  //adding this
+  else if (++blink_count == 0 && button_state == 1) {
+    state_advance();
+    blink_count = 0;
+    buzzer_set_period(0);
+  }
+  
+
+
+
+  
 }
