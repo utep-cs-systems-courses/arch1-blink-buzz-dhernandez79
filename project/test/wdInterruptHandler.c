f55e2bdc86fa29eae0c0a924c412cd7e782a62d1
Demetrius Hernandez <demy.hernandez00@gmail.com>
2021-04-08 21:46:51 -0600

    small edits

--- project/test/wdInterruptHandler.c
+++ project/test/wdInterruptHandler.c
@@ -15,15 +15,15 @@ __interrupt_vec(WDT_VECTOR) WDT(){/* 250 interrupts/sec */
     blink_count = 0;
     buzzer_set_period(1000);
   }
-  //adding this
   else if (++blink_count == 0 && button_state == 1) {
-    play_police(); //plays jaws theme
+    play_police(); //plays police siren
     blink_count = 0;
   }
   else if (++blink_count == 0 && button_state == 2) {
     play_twinkle();//plays twinkle twinkle little star
     blink_count++;
   }
+  //if last button is pressed it will freeze on the current state
   
 
 
